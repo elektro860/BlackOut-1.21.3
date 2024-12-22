ac3d034da2bd3cc78b2f54816fb4baf19de4fa58
elektro <67273104+elektro860@users.noreply.github.com>
2024-12-22 17:37:35 +0100

    I stalined some code

--- src/main/java/kassuk/addon/blackout/mixins/MixinBackground.java
+++ src/main/java/kassuk/addon/blackout/mixins/MixinBackground.java
@@ -4,6 +4,8 @@ import kassuk.addon.blackout.modules.Fog;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
+
+import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,8 +13,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BackgroundRenderer.class)
 public class MixinBackground {
-    @Inject(method = "applyFog", at = @At("TAIL"))
-    private static void applyFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo info) {
+    // @Inject(method =
+    // "applyFog(Lnet/minecraft/client.render/Camera;Lnet/minecraft/client/render/BackgroundRenderer/FogType;Lorg/joml/Vector4f;F;Z;F;)Lnet/minecraft/client/render/Fog;",
+    // at = @At("TAIL"))
+    // @Inject(method =
+    // "applyFog(Lnet/minecraft/client/render/Camera;Lnet/minecraft/client/render/BackgroundRenderer/FogType;Lorg/joml/Vector4f;FZF)Lnet/minecraft/client/render/Fog;",
+    // at = @At("TAIL"))
+    private static void applyFog(Camera camera, BackgroundRenderer.FogType fogType, Vector4f color, float viewDistance,
+            boolean thickenFog, float tickDelta, CallbackInfo i) {
         Fog fog = Modules.get().get(Fog.class);
         if (fog != null && fog.isActive() && fogType == BackgroundRenderer.FogType.FOG_TERRAIN) {
             fog.modifyFog();
--- src/main/java/kassuk/addon/blackout/mixins/MixinClientPlayerEntity.java
+++ src/main/java/kassuk/addon/blackout/mixins/MixinClientPlayerEntity.java
@@ -51,27 +51,46 @@ public abstract class MixinClientPlayerEntity {
         }
     }
 
-    @Redirect(method = "sendMovementPackets", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayNetworkHandler;sendPacket(Lnet/minecraft/network/packet/Packet;)V", ordinal = 2))
-    private void sendPacketFull(ClientPlayNetworkHandler instance, Packet<?> packet) {
-        networkHandler.sendPacket(Managers.ROTATION.onFull((PlayerMoveC2SPacket.Full) packet));
-    }
+    // One of these is crashing the game and they don't smee to be doing anything
+    // interesting
+    // @Redirect(method = "sendMovementPackets", at = @At(value = "INVOKE", target =
+    // "Lnet/minecraft/client/network/ClientPlayNetworkHandler;sendPacket(Lnet/minecraft/network/packet/Packet;)V",
+    // ordinal = 2))
+    // private void sendPacketFull(ClientPlayNetworkHandler instance, Packet<?>
+    // packet) {
+    // networkHandler.sendPacket(Managers.ROTATION.onFull((PlayerMoveC2SPacket.Full)
+    // packet));
+    // }
 
-    @Redirect(method = "sendMovementPackets", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayNetworkHandler;sendPacket(Lnet/minecraft/network/packet/Packet;)V", ordinal = 3))
-    private void sendPacketPosGround(ClientPlayNetworkHandler instance, Packet<?> packet) {
-        networkHandler
-                .sendPacket(Managers.ROTATION.onPositionOnGround((PlayerMoveC2SPacket.PositionAndOnGround) packet));
-    }
+    // @Redirect(method = "sendMovementPackets", at = @At(value = "INVOKE", target =
+    // "Lnet/minecraft/client/network/ClientPlayNetworkHandler;sendPacket(Lnet/minecraft/network/packet/Packet;)V",
+    // ordinal = 3))
+    // private void sendPacketPosGround(ClientPlayNetworkHandler instance, Packet<?>
+    // packet) {
+    // networkHandler
+    // .sendPacket(Managers.ROTATION.onPositionOnGround((PlayerMoveC2SPacket.PositionAndOnGround)
+    // packet));
+    // }
 
-    @Redirect(method = "sendMovementPackets", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayNetworkHandler;sendPacket(Lnet/minecraft/network/packet/Packet;)V", ordinal = 4))
-    private void sendPacketLookGround(ClientPlayNetworkHandler instance, Packet<?> packet) {
-        PlayerMoveC2SPacket toSend = Managers.ROTATION.onLookAndOnGround((PlayerMoveC2SPacket.LookAndOnGround) packet);
-        if (toSend != null) {
-            networkHandler.sendPacket(toSend);
-        }
-    }
+    // @Redirect(method = "sendMovementPackets", at = @At(value = "INVOKE", target =
+    // "Lnet/minecraft/client/network/ClientPlayNetworkHandler;sendPacket(Lnet/minecraft/network/packet/Packet;)V",
+    // ordinal = 4))
+    // private void sendPacketLookGround(ClientPlayNetworkHandler instance,
+    // Packet<?> packet) {
+    // PlayerMoveC2SPacket toSend =
+    // Managers.ROTATION.onLookAndOnGround((PlayerMoveC2SPacket.LookAndOnGround)
+    // packet);
+    // if (toSend != null) {
+    // networkHandler.sendPacket(toSend);
+    // }
+    // }
 
-    @Redirect(method = "sendMovementPackets", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayNetworkHandler;sendPacket(Lnet/minecraft/network/packet/Packet;)V", ordinal = 5))
-    private void sendPacketGround(ClientPlayNetworkHandler instance, Packet<?> packet) {
-        networkHandler.sendPacket(Managers.ROTATION.onOnlyOnground((PlayerMoveC2SPacket.OnGroundOnly) packet));
-    }
+    // @Redirect(method = "sendMovementPackets", at = @At(value = "INVOKE", target =
+    // "Lnet/minecraft/client/network/ClientPlayNetworkHandler;sendPacket(Lnet/minecraft/network/packet/Packet;)V",
+    // ordinal = 5))
+    // private void sendPacketGround(ClientPlayNetworkHandler instance, Packet<?>
+    // packet) {
+    // networkHandler.sendPacket(Managers.ROTATION.onOnlyOnground((PlayerMoveC2SPacket.OnGroundOnly)
+    // packet));
+    // }
 }
